a25860e07bb6bf8be07ad58400db0d8a9517618a
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2017-01-07 19:37:10 +0800

    updated

--- firenews.go
+++ firenews.go
@@ -425,7 +425,7 @@ func ActiveAllElements(elements []RssItem) []RssItem {
 
 // CleanupElements makes elements clean
 func CleanupElements(elements []RssItem) []RssItem {
-	keywords := "關鍵字搜尋搜尋|行善|廟|寺|地震.*局勢|局勢.*地震|地震.*經濟|經濟.*地震|收成|收成|價格|貸款|保險|價揚|治安|人壽|訂單|投資|火燒心|daily|價高|價低|量多|量少|民調|祝福"
+	keywords := "關鍵字搜尋搜尋|行善|廟|寺|地震.*局勢|局勢.*地震|地震.*經濟|經濟.*地震|收成|收成|價格|貸款|保險|價揚|治安|人壽|訂單|投資|火燒心|daily|價高|價低|量多|量少|民調|希望"
 	rp := regexp.MustCompile(keywords)
 
 	for i := len(elements) - 1; i >= 0; i-- {
